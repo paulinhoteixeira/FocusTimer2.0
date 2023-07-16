12cd3ccd853d931a552718f97c441a81559865fc
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2023-07-15 22:07:28 -0300

    countdown function

--- script.js
+++ script.js
@@ -8,6 +8,35 @@ let seconds = document.querySelector(".seconds");
 
 console.log(minutes);
 
+function countdown() {
+  let newSeconds = Number(seconds.textContent);
+  let newMinutes = Number(minutes.textContent);
+
+  setTimeout(() => {
+    
+    if (newSeconds == 0 && newMinutes == 0) {
+      seconds.textContent = String(newSeconds).padStart(2, "0");
+      
+      return;
+    }
+    
+    if (newSeconds == 0) {
+      newMinutes = newMinutes - 1;
+      newSeconds = 60;
+    }
+    
+    newSeconds = newSeconds - 1;
+    seconds.textContent = String(newSeconds).padStart(2, "0");
+    minutes.textContent = String(newMinutes).padStart(2, "0");
+
+    countdown();
+  }, 1000);
+}
+
+buttonPlay.addEventListener("click", function () {
+  countdown();
+});
+
 buttonMore.addEventListener("click", function () {
   let newMinutes = Number(minutes.textContent) + 5;
   minutes.textContent = String(newMinutes).padStart(2, "0");
