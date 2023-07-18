e53cc75b7909660ed0b5558a9eeb9c88c616daf5
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2023-07-17 22:19:55 -0300

    stop and pause buttons functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import pressButton from "./sounds.js"
 const buttonPlay = document.querySelector(".play");
 const buttonPause = document.querySelector(".pause");
 const buttonStop = document.querySelector(".stop");
@@ -6,6 +7,7 @@ const buttonMinus = document.querySelector(".minus5Minutes");
 
 let minutes = document.querySelector(".minutes");
 let seconds = document.querySelector(".seconds");
+let time
 
 console.log(minutes);
 
@@ -13,32 +15,52 @@ function countdown() {
   let newSeconds = Number(seconds.textContent);
   let newMinutes = Number(minutes.textContent);
 
-  setTimeout(() => {
-    
+  time = setTimeout(() => {
     if (newSeconds == 0 && newMinutes == 0) {
       seconds.textContent = String(newSeconds).padStart(2, "0");
-      
+
       return;
     }
-    
+
     if (newSeconds == 0) {
       newMinutes = newMinutes - 1;
       newSeconds = 60;
     }
-    
+
     newSeconds = newSeconds - 1;
     seconds.textContent = String(newSeconds).padStart(2, "0");
     minutes.textContent = String(newMinutes).padStart(2, "0");
 
     countdown();
   }, 1000);
+
+  
+}
+
+function resetTimer() {
+  clearTimeout(time)
+  minutes.textContent = 25;
+  seconds.textContent = String(0).padStart(2, "0");
 }
 
 buttonPlay.addEventListener("click", function () {
-  buttonPlay.classList.add("hide")
-  buttonPause.classList.remove("hide")
-
+  buttonPlay.classList.add("hide");
+  buttonPause.classList.remove("hide");
+  pressButton()
   countdown();
+});
+
+buttonPause.addEventListener("click", function () {
+  buttonPlay.classList.remove("hide");
+  buttonPause.classList.add("hide");
+  clearTimeout(time)
+
+});
+
+buttonStop.addEventListener("click", function () {
+  buttonPlay.classList.remove("hide");
+  buttonPause.classList.add("hide");
+  resetTimer();
 });
 
 buttonMore.addEventListener("click", function () {
